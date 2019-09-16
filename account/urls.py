9013233b467c5ed78261826b95ec8d1b48ca41b6
nonoike <ij4nu8d4fw@gmail.com>
2019-09-16 15:31:34 +0900

    [update] use the custom user model when signup

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
+from account.views import RegisterView
 from . import views
 
 app_name = "account"
@@ -8,4 +9,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', LoginView.as_view(template_name='account/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='account/index.html'), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from account.forms import RegisterForm
+
 
 def index(request):
     return render(request, 'account/index.html')
+
+
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    success_url = reverse_lazy('account:index')
+    template_name = 'account/register.html'
