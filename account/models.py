e5ae454fc56775981d72fc332f64d23909284c2f
nonoike <ij4nu8d4fw@gmail.com>
2019-09-16 14:05:46 +0900

    [update] use the custom user model when login and logout

--- account/models.py
+++ account/models.py
@@ -55,4 +55,3 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     class Meta:
         db_table = 'users'
-
--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from . import views
 
+app_name = "account"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', LoginView.as_view(template_name='account/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='account/index.html'), name='logout'),
 ]
