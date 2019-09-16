ba1b5113bf287cb9b898633e86baea1574f5333b
nonoike <ij4nu8d4fw@gmail.com>
2019-09-16 16:34:54 +0900

    [update] use the custom user model when change the password

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
+from django.urls import path, reverse_lazy
 
 from account.views import RegisterView
 from . import views
@@ -9,5 +9,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', LoginView.as_view(template_name='account/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='account/index.html'), name='logout'),
+    path('password_change/', PasswordChangeView.as_view(success_url=reverse_lazy('account:index'),
+                                                        template_name='account/password_change.html'),
+         name='password_change'),
+    # path('password_change_done/', views.password_change_done, name='password_change_done'),
     path('register/', RegisterView.as_view(), name='register'),
 ]
--- initialize.sh
+++ initialize.sh
@@ -8,4 +8,5 @@ docker-compose up -d --build
 
 docker exec django_account python /workspace/django_account/manage.py makemigrations account
 docker exec django_account python /workspace/django_account/manage.py migrate
-docker exec django_account python /workspace/django_account/manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('hogehoge@mail', 'hogehoge')"
+docker exec django_account python /workspace/django_account/manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin@mail', 'hogehoge')"
+docker exec django_account python /workspace/django_account/manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_user('user@mail', 'hogehoge')"
